ac2a3b3ffe7a85fc70dfe41474dd7ff8aba098e9
HelloSeva <sevda.seyidova611@gmail.com>
2023-01-24 11:01:41 +0000

    local storage and click function to save what is written in the text area fixed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,3 +19,24 @@ for (var i = 9; i < 18; i++) {
   }
 }
 
+//click function to save what is written in the text area into local storage
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+
+  //local storage //text remains after user refreshes the page
+  $("#row-9 .description").val(localStorage.getItem("row-9"));
+  $("#row-10 .description").val(localStorage.getItem("row-10"));
+  $("#row-11 .description").val(localStorage.getItem("row-11"));
+  $("#row-12 .description").val(localStorage.getItem("row-12"));
+  $("#row-13 .description").val(localStorage.getItem("row-13"));
+  $("#row-14 .description").val(localStorage.getItem("row-14"));
+  $("#row-15 .description").val(localStorage.getItem("row-15"));
+  $("#row-16 .description").val(localStorage.getItem("row-16"));
+  $("#row-17 .description").val(localStorage.getItem("row-17"));
+});
+
